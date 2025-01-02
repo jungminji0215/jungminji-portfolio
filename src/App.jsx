6d65da8100bbc415c8611d3c8cbef12361ab338a
jungminji <wjdalswl0215@gmail.com>
2025-01-02 14:30:35 +0900

    레이아웃

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import "./App.css";
 
-function App() {}
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="cover cover-left"></div>
+      <div className="cover cover-right turn"></div>
+    </div>
+  );
+}
 
 export default App;
