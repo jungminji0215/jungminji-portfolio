00c8d0037a54a3312713b98531e75141e96a2d98
jungminji <wjdalswl0215@gmail.com>
2025-01-10 19:48:50 +0900

    header 구성

--- src/App.jsx
+++ src/App.jsx
@@ -2,93 +2,123 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="main">
-      <header className="">
-        <h1>Jungminji</h1>
+    <>
+      {/* Header */}
+      <header>
+        <div>
+          <h1>
+            <a href="#">Jungminji</a>
+          </h1>
+        </div>
         <nav>
-          <span>About Me</span>
-          <span>Skills</span>
-          <span>Projects</span>
-          <span>Career</span>
-          <span>Education</span>
-          <span>Contact Me</span>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about-me">About Me</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#career">Career</a>
+            </li>
+            <li>
+              <a href="#education">Education</a>
+            </li>
+            <li>
+              <a href="#contact-me">Contact Me</a>
+            </li>
+          </ul>
         </nav>
       </header>
 
-      {/* 진입 */}
-      <section className="section-A">
-        <span>방문 감사합니다.</span>
-        <div>
-          {/* TODO 여러 단어 */}
-          <span>프론트엔드 개발자</span>
-          <span>정민지입니다.</span>
-        </div>
-        <div>스크롤</div>
-      </section>
+      {/* Main */}
+      <main>
+        {/* Home */}
+        <section id="home">
+          <div>
+            {/* TODO 여러 단어 */}
+            <span>프론트엔드 개발자</span>
+            <span>정민지입니다.</span>
+          </div>
+          <div>스크롤</div>
+        </section>
 
-      {/* about me */}
-      <section>
-        <h2>About Me</h2>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum nobis
-        eaque unde. Temporibus id dolorum atque fugiat eaque, vel itaque.
-        Suscipit blanditiis consectetur nulla voluptatum, nemo provident cumque
-        ea veniam?
-      </section>
+        {/* About Me */}
+        <section id="about-me">
+          <h2>About Me</h2>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
+          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
+          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
+          provident cumque ea veniam?
+        </section>
 
-      {/* skills */}
-      <section>
-        <h2>Skills</h2>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum nobis
-        eaque unde. Temporibus id dolorum atque fugiat eaque, vel itaque.
-        Suscipit blanditiis consectetur nulla voluptatum, nemo provident cumque
-        ea veniam?
-      </section>
+        {/* Skills */}
+        <section id="skills">
+          <h2>Skills</h2>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
+          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
+          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
+          provident cumque ea veniam?
+        </section>
 
-      {/* project */}
-      <section>
-        <h2>Projects</h2>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum nobis
-        eaque unde. Temporibus id dolorum atque fugiat eaque, vel itaque.
-        Suscipit blanditiis consectetur nulla voluptatum, nemo provident cumque
-        ea veniam?
-      </section>
+        {/* Project */}
+        <section id="projects">
+          <h2>Projects</h2>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
+          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
+          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
+          provident cumque ea veniam?
+        </section>
 
-      {/* Career */}
-      <section>
-        <h2>Career</h2>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum nobis
-        eaque unde. Temporibus id dolorum atque fugiat eaque, vel itaque.
-        Suscipit blanditiis consectetur nulla voluptatum, nemo provident cumque
-        ea veniam?
-      </section>
+        {/* Career */}
+        <section id="career">
+          <h2>Career</h2>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
+          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
+          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
+          provident cumque ea veniam?
+        </section>
 
-      {/* education */}
-      <section>
-        <h2>Education</h2>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum nobis
-        eaque unde. Temporibus id dolorum atque fugiat eaque, vel itaque.
-        Suscipit blanditiis consectetur nulla voluptatum, nemo provident cumque
-        ea veniam?
-      </section>
+        {/* Education */}
+        <section id="education">
+          <h2>Education</h2>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
+          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
+          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
+          provident cumque ea veniam?
+        </section>
 
-      {/* contact me */}
-      <section>
-        <h2>Contact Me</h2>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum nobis
-        eaque unde. Temporibus id dolorum atque fugiat eaque, vel itaque.
-        Suscipit blanditiis consectetur nulla voluptatum, nemo provident cumque
-        ea veniam?
-      </section>
+        {/* Contact Me */}
+        <section id="contact-me">
+          <h2>Contact Me</h2>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
+          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
+          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
+          provident cumque ea veniam?
+        </section>
 
-      {/* footer */}
-      <section>
-        <h2>About Me</h2>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum nobis
-        eaque unde. Temporibus id dolorum atque fugiat eaque, vel itaque.
-        Suscipit blanditiis consectetur nulla voluptatum, nemo provident cumque
-        ea veniam?
-      </section>
-    </div>
+        {/* footer */}
+        <section>
+          <h2>About Me</h2>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
+          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
+          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
+          provident cumque ea veniam?
+        </section>
+
+        {/* Top Scroll */}
+        <aside></aside>
+      </main>
+
+      {/* Footer */}
+      <footer></footer>
+    </>
   );
 }
 
