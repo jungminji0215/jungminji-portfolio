9d7f206679b822606ba9ea5bcc9f7915a1bd4ff7
jungminji <wjdalswl0215@gmail.com>
2025-01-10 22:42:47 +0900

    html 정보 구성
    - about me
    - skills
    - projects
    - career

--- src/App.jsx
+++ src/App.jsx
@@ -41,48 +41,136 @@ function App() {
       <main>
         {/* Home */}
         <section id="home">
-          <div>
-            {/* TODO 여러 단어 */}
-            <span>프론트엔드 개발자</span>
-            <span>정민지입니다.</span>
-          </div>
-          <div>스크롤</div>
+          <p>안녕하세요, 프론트엔드 개발자 정민지입니다.</p>
+          <a href="#about-me">
+            <i
+              className="bx bx-chevrons-down bx-flip-horizontal bx-tada"
+              style={{ color: "#5db86b" }}
+            ></i>
+          </a>
         </section>
 
         {/* About Me */}
         <section id="about-me">
           <h2>About Me</h2>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
-          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
-          provident cumque ea veniam?
+          <div>
+            {/* 소개 */}
+            <section>
+              <p>
+                안녕하세요 저는 이러이러한 개발자입니다. 이러이러한 개발자가
+                되고싶어요
+              </p>
+            </section>
+
+            <section>
+              <a href="https://github.com/jungminji0215" target="_blank">
+                <i className="fa-brands fa-github"></i>
+              </a>
+
+              <a
+                href="https://jungminji0215.tistory.com/"
+                target="_blank"
+                className="test"
+              >
+                <i
+                  className="fa-solid fa-blog"
+                  style={{ color: " #dd7631" }}
+                ></i>
+              </a>
+
+              <div>
+                <i
+                  className="fa-regular fa-envelope"
+                  style={{ color: "#d06c71" }}
+                ></i>
+              </div>
+
+              <div>
+                <i
+                  className="fa-regular fa-file"
+                  style={{ color: "#989a98" }}
+                ></i>
+              </div>
+            </section>
+          </div>
         </section>
 
         {/* Skills */}
         <section id="skills">
           <h2>Skills</h2>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
-          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
-          provident cumque ea veniam?
+          <div>
+            <ul>
+              <li>
+                <i className="fa-brands fa-html5"></i>
+                <p>Frontend</p>
+                <p>HTML, CSS, Javascript, Typescript, React, Next.js</p>
+              </li>
+              <li>
+                <i className="fa-solid fa-server"></i>
+                <p>Backend</p>
+                <p>SpringBoot, Node.js</p>
+              </li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
         </section>
 
-        {/* Project */}
+        {/* Projects */}
         <section id="projects">
           <h2>Projects</h2>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
-          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
-          provident cumque ea veniam?
+          <ul>
+            <li>포트폴리오 사이트</li>
+            <li>기술 면접 사이트</li>
+          </ul>
         </section>
 
         {/* Career */}
         <section id="career">
           <h2>Career</h2>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
-          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
-          provident cumque ea veniam?
+          <ul className="workeduc-box">
+            <li className="workeduc-content">
+              {/* TODO 회사 로고 */}
+              {/* <img src="" alt="bomapp logo" /> */}
+              <span className="year">
+                <i className="bx bxs-calendar"></i>2020 - 2021
+              </span>
+              <h3>웹 개발자 회사</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil
+                iste voluptas voluptate sed. Delectus cupiditate, tempora, fugit
+                iste deserunt amet perspiciatis obcaecati quam distinctio optio
+                ex aperiam accusantium magni qui?
+              </p>
+            </li>
+
+            <li className="workeduc-content">
+              <span className="year">
+                <i className="bx bxs-calendar"></i>2020 - 2021
+              </span>
+              <h3>웹 개발자 회사</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil
+                iste voluptas voluptate sed. Delectus cupiditate, tempora, fugit
+                iste deserunt amet perspiciatis obcaecati quam distinctio optio
+                ex aperiam accusantium magni qui?
+              </p>
+            </li>
+
+            <li className="workeduc-content">
+              <span className="year">
+                <i className="bx bxs-calendar"></i>2020 - 2021
+              </span>
+              <h3>웹 개발자 회사</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil
+                iste voluptas voluptate sed. Delectus cupiditate, tempora, fugit
+                iste deserunt amet perspiciatis obcaecati quam distinctio optio
+                ex aperiam accusantium magni qui?
+              </p>
+            </li>
+          </ul>
         </section>
 
         {/* Education */}
@@ -103,21 +191,18 @@ function App() {
           provident cumque ea veniam?
         </section>
 
-        {/* footer */}
-        <section>
-          <h2>About Me</h2>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-          nobis eaque unde. Temporibus id dolorum atque fugiat eaque, vel
-          itaque. Suscipit blanditiis consectetur nulla voluptatum, nemo
-          provident cumque ea veniam?
-        </section>
-
         {/* Top Scroll */}
-        <aside></aside>
+        <aside>
+          <a href="#" title="위로 가기">
+            <i className="fa-solid fa-arrow-up"></i>
+          </a>
+        </aside>
       </main>
 
       {/* Footer */}
-      <footer></footer>
+      <footer>
+        <p>Copyright 2024.Jung Minji.All rights rederved.</p>
+      </footer>
     </>
   );
 }
